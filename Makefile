4f0099c09abb06b213915990e3b94c4d47648f0c
Kevin Benard <mail@kevinbenard.com>
2018-01-16 22:40:28 -0600

    Adding support for priority on todo item

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 CC=clang
-CFLAGS=-Wall -Wextra -Wpedantic
+CFLAGS=-Wall -Wextra -Wpedantic -g
 
 all: todo
 
 todo: todo.c
 	$(CC) $(CFLAGS) todo.c -o todo
+
+run: todo
+	rm ./data.db
+	./todo one two three
+	./todo 4 five six seven
+	./todo -l
+
+clean:
+	rm -rf ./todo ./todo.dSYM/
--- todo.c
+++ todo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#include <ctype.h>
 
 #define COMMENT_SIZE 127
 
@@ -24,17 +25,19 @@ int main(int argc, char** argv) {
     uint8_t index = 0;
     // TODO: Replace with fread()
     while(fgets(buf, sizeof(buf), mainFile) != NULL) {
-        todo_items[index].index = index;
+        todo_items[index].index = index + 1;
         char* comment = malloc(sizeof(buf));
-        comment = strncpy(comment, buf, sizeof(buf));
-
+        comment = strncpy(comment, buf+4, sizeof(buf));
         todo_items[index].comment = comment;
-        todo_items[index].priority = 0;
+
+        char* pri = malloc(4 * sizeof(buf[0]));
+        pri = strncpy(pri, buf+1, 1);
+        todo_items[index].priority = atoi(pri);
         index += 1;
     }
 
     if (feof(mainFile)) {
-        printf("Finished!\n");
+        // printf("Finished!\n");
     }
 
     fclose(mainFile);
@@ -42,19 +45,33 @@ int main(int argc, char** argv) {
     if (argc == 2 && strncmp(argv[1], "-l", 2) == 0) {
         // Print items
         for (int i = 0; i < index; i++) {
-            printf("%d. [%d]: %s\n", i, todo_items[i].priority, todo_items[i].comment);
+            printf("%d. [%d]: %s",
+                    todo_items[i].index, 
+                    todo_items[i].priority, 
+                    todo_items[i].comment);
         }
     } else if (argc >= 2) {
         // Append todo to files + array
         int start = 0;
         char line[255]; // TODO: Do something better than 255
+        int priority = 0;
+        if (isdigit(argv[1][0])) {
+            priority = atoi(argv[1]);
+            argv++;
+            argc--;
+        }
+
+        sprintf(&line[start], "[%d] ", priority); 
+        start += 4;
+        // TODO: Make this support double-digit priorities
+        // TODO: Only shift argv if there was a priority specified
         for (int i = 1; i < argc; i++) {
             int len = strlen(argv[i]);
             sprintf(&line[start], "%s%s", argv[i], " ");
             start += len + 1;
         }
         printf("Line: %s\n", line);
-        printf("Size: %d\n", argc);
+        // printf("Size: %d\n", argc);
 
         mainFile = fopen("data.db", "a+");
         if (!mainFile) {
